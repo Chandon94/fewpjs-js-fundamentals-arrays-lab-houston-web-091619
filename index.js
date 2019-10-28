cd9cade2d045b5c9255b785a82e86182bd36c5a5
Chandon Porter <cneilporter94@gmail.com>
2019-10-28 03:23:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const append = ['Milo','Otis','Garfield']
+append.push('Odie')
+
+const prepend = ['Milo','Otis','Garfield']
+prepend.unshift('Otis')
+
+const removeLast = ['Milo','Otis','Garfield']
+removeLast.pop('Garfield')
+
+const removeFirst = ['Milo','Otis','Garfield']
+removeFirst.shift()
